745f0356bfb9d5595973618c328275d1c378ca31
kocapirireis <pirireis.kumanya@gmail.com>
2026-03-08 20:15:29 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    // Google Apps Script Deploy sonrasında verilen Web App URL'i
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyX2JTQb-VLV2tCMdJjyhukR0JEzunGBSFFlcbuTuIZFYjlNd1ivkbE98Nvk7UzBVfJRQ/exec"
+};
