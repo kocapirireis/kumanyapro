5cae6e563b43512bedae0eba69663b571ba82cc7
kocapirireis <pirireis.kumanya@gmail.com>
2026-03-10 20:31:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,7 @@ const CONFIG = {
     // Google Apps Script Deploy sonrasında verilen Web App URL'i
     APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyX2JTQb-VLV2tCMdJjyhukR0JEzunGBSFFlcbuTuIZFYjlNd1ivkbE98Nvk7UzBVfJRQ/exec"
 };
+
+
+
+
