f8aa0a64437d6f5cbd59ccb20c8c9a45456b8d75
kocapirireis <pirireis.kumanya@gmail.com>
2026-03-15 13:52:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const CONFIG = {
     // Google Apps Script Deploy sonrasında verilen Web App URL'i
-    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyX2JTQb-VLV2tCMdJjyhukR0JEzunGBSFFlcbuTuIZFYjlNd1ivkbE98Nvk7UzBVfJRQ/exec"
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyX2JTQb-VLV2tCMdJjyhukR0JEzunGBSFFlcbuTuIZFYjlNd1ivkbE98Nvk7UzBVfJRQ/exec",
+    
+    // Supabase Credentials
+    SB_URL: 'https://mrwqkwarsyhoyyjhxlsz.supabase.co',
+    SB_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yd3Frd2Fyc3lob3l5amh4bHN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMDEyMDUsImV4cCI6MjA4ODU3NzIwNX0.YlhTmbK7sF8U_-7VhQKDJtQ80B3C78MtJrDX5IvES9k'
 };
-
-
